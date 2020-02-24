26d653b0a740d607eb77267a171cda3e15939d2f
Andrew Hinojosa <drews_view@hotmail.com>
2020-02-24 02:21:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+require 'pry'
+
+def prime?(integer)
+
+  prime = integer % 1 == 0
+  not_prime = integer % 1 != 0
+  
+  if integer == prime
+      true
+    else
+      false
+  end
+end
