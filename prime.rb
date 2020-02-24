b379794476a0ac2d112dbcbbf6f0f226fdc4f0f7
Andrew Hinojosa <drews_view@hotmail.com>
2020-02-24 02:36:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,7 +5,7 @@ def prime?(integer)
   if num <= 0 || num >= 0
       false
     else
-      (-1..1)to_a.all? do |num|
+      (-Infinity..Infinity)to_a.all? do |num|
         num % integer != 0
   end
 end
